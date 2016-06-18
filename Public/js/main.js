85dba48d6625dd09a0d5a60cbec04a60487aae5f
Anthony_box <york_mail@qq.com>
2016-06-18 20:41:42 +0800

    新增时间格式化函数

--- Public/js/main.js
+++ Public/js/main.js
@@ -1,16 +1,17 @@
 /**
  * Created by anthony on 16/6/15.
  */
-$(document).ready(function(){
+var NOW_PAGE;//全局
+$(document).ready(function () {
     var menu = $(".list-menu");
     var func = $(".func");
 
     func.first().fadeIn();
-    menu.click(function(ev){
+    menu.click(function (ev) {
         ev.stopPropagation();
         menu.removeClass("active");
         var to = this.getAttribute("data-to");
-        $("[data-to='"+to+"']").addClass("active");
+        $("[data-to='" + to + "']").addClass("active");
         func.fadeOut(0);
         func.eq(to).fadeIn(0);
     });
@@ -19,19 +20,33 @@ $(document).ready(function(){
         //format: "dd MM yyyy - hh:ii"
         format: "yyyy/MM/dd",
         language: 'zh-CN',
-        autoclose:true,
+        autoclose: true,
     });
 });
-function loadMore(target,url,page){
+function loadMore(target, url, page) {
     $.ajax({
         type: 'POST',
         url: url,
-        data:{"do":"loadmore","page":page},
+        data: {"do": "loadmore", "page": page},
         success: function (data) {
-            if(data){
+            if (data) {
                 console.log(data);
                 $(target).html(data.table);
             }
         }
     });
+}
+function formatDate(timestamp) {
+    var date = new Date(parseInt(timestamp) * 1000);
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var hour = date.getHours();
+    var minute = date.getMinutes();
+    var second = date.getSeconds();
+    var retString = year + "/" + month + "/" + day;
+    if (hour != 0 && minute != 0 && second != 0) {
+        retString += " " + hour + ":" + minute + ":" + second
+    }
+    return retString;
 }
